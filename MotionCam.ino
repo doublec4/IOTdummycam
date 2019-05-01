071d4506778c7b4f910e8fe7cd3a837ce8f12ca4
doublec4 <chris.chimienti@gmail.com>
2019-05-01 14:26:25 -0400

    Update MotionCam.ino

--- MotionCam.ino
+++ MotionCam.ino
@@ -74,44 +74,7 @@ void setup() {
   
   delay(15000); //wait for PIR sensor to stabilize
 
-/*
-  EEPROM.begin(512);
-  // get the current count position from eeprom
-  byte battery_count = EEPROM.read(0);
 
-  // we only need this to happen once every X minutes,
-  // so we use eeprom to track the count between resets.
-  if(battery_count >= ((BATTERY_INTERVAL * 60) / SLEEP_LENGTH)) {
-    // reset counter
-    battery_count = 0;
-    // report battery level to Adafruit IO
-    battery_level();
-  }
-  else {
-    // increment counter
-    battery_count++;
-  }
-
-  // save the current count
-  EEPROM.write(0, battery_count);
-  EEPROM.commit();
-
-  // if door isn't open, we don't need to send anything
-  if(digitalRead(DOOR) == LOW) {
-    Serial.println("Door closed");
-    // we don't do anything
-  }
-  else {
-    // the door is open if we have reached here,
-    // so we should send a value to Adafruit IO.
-    Serial.println("Door is open!");
-    door_open();
-  }
-
-  // we are done here. go back to sleep.
-  Serial.println("zzzz");
-  ESP.deepSleep(SLEEP_LENGTH * 1000000);
-  */
 }
 
 
@@ -157,29 +120,3 @@ void loop(){
   
 }
 
-
-
-/*
-void battery_level() {
-  // read the battery level from the ESP8266 analog in pin.
-  // analog read level is 10 bit 0-1023 (0V-1V).
-  // our 1M & 220K voltage divider takes the max
-  // lipo value of 4.2V and drops it to 0.758V max.
-  // this means our min analog read value should be 580 (3.14V)
-  // and the max analog read value should be 774 (4.2V).
-  int level = analogRead(A0);
-
-  // convert battery level to percent
-  level = map(level, 580, 774, 0, 100);
-  Serial.print("Battery level: "); Serial.print(level); Serial.println("%");
-
-  // connect us to Adafruit IO
-  connect_AIO();
-  // grab the battery feed
-  AdafruitIO_Feed *battery = io.feed("battery");
-  
-  // send battery level to AIO
-  battery->save(level);
-  io.run();
-}
-*/
